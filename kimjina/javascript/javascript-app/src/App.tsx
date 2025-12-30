d9195c94cbc589c42b64908195c32ab33c56a865
try14190-code <try14190@gmail.com>
2025-12-30 13:38:49 +0900

    feat: [김진아] Math 기능 및 switch 키워드 활용 [PMIF-x]

--- kimjina/javascript/javascript-app/src/App.tsx
+++ kimjina/javascript/javascript-app/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { ControlFlowIf } from './control_flow/ControlFlowIf'
+import { ControlFlowSwitch } from './control_flow/ControlFlowIf Switch'
 import { VariableAndDataTypes } from './data_type/VariableAndDataTypes'
 import { LogicalOperationExample } from './logical_operation/LogicalOperationExample'
 import { CalculationExample } from './math_operation/calculationExample'
@@ -20,7 +21,7 @@ function App() {
         <LogicalOperationExample/>
         <ControlFlowIf/>
         <FirstProblem/>
-        
+        <ControlFlowSwitch/>
         
       </div>
       
